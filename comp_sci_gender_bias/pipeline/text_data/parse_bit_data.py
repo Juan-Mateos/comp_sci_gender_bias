7e22e2fdd59d4876a524d55377b23e152f7ab0a3
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-05-03 13:43:29 +0100

    change script

--- comp_sci_gender_bias/pipeline/text_data/parse_bit_data.py
+++ comp_sci_gender_bias/pipeline/text_data/parse_bit_data.py
@@ -21,21 +21,23 @@ def getText(filename: str) -> str:
     return " ".join(fullText).strip()
 
 
-for dir_, name in zip(BIT_DIRS, ["compsci", "geo"]):
-    logging.info(f"parsing {name}")
-    descr_path = os.path.join(DATA_DIR, dir_)
-    print(descr_path)
-    docx_files = os.listdir(descr_path)
+if __name__ == "__main__":
 
-    # In this dict the keys are schools ids and the values the text descriptions
-    # of a subject
-    text_dict = {
-        file_name.split("_")[0]: getText(f"{descr_path}/{file_name}")
-        for file_name in docx_files
-    }
+    for dir_, name in zip(BIT_DIRS, ["compsci", "geo"]):
+        logging.info(f"parsing {name}")
+        descr_path = os.path.join(DATA_DIR, dir_)
+        print(descr_path)
+        docx_files = os.listdir(descr_path)
 
-    with open(
-        f"{PROJECT_DIR}/inputs/data/{name}_descr.json",
-        "w",
-    ) as outfile:
-        json.dump(text_dict, outfile, indent=1)
+        # In this dict the keys are schools ids and the values the text descriptions
+        # of a subject
+        text_dict = {
+            file_name.split("_")[0]: getText(f"{descr_path}/{file_name}")
+            for file_name in docx_files
+        }
+
+        with open(
+            f"{PROJECT_DIR}/inputs/data/{name}_descr.json",
+            "w",
+        ) as outfile:
+            json.dump(text_dict, outfile, indent=1)
