8123560043811fc091bbce295ce530f3a0e96bc4
lizgzil <lizgzil@hotmail.com>
2022-05-09 14:22:11 +0100

    Allow option to convert proper nouns to nouns

--- comp_sci_gender_bias/pipeline/process_text_utils.py
+++ comp_sci_gender_bias/pipeline/process_text_utils.py
@@ -7,12 +7,19 @@ class TokenTagger:
         spacy_udpipe.download("en")
         self.nlp = spacy_udpipe.load("en")
 
-    def tag(self, sentence):
+    def tag(self, sentence, convert_propn=True):
         """
         Tokenise a single sentence and output a list of tuples
         (text, lemma, POS) for each token
+
+        If convert_propn==True then convert all proper nouns to nouns
         """
 
         doc = self.nlp(sentence)
 
-        return [(token.text, token.lemma_, token.pos_) for token in doc]
+        tags = [(token.text, token.lemma_, token.pos_) for token in doc]
+
+        if convert_propn:
+            return [(t, l, "NOUN") if p == "PROPN" else (t, l, p) for (t, l, p) in tags]
+        else:
+            return tags
--- comp_sci_gender_bias/tests/test_process_text.py
+++ comp_sci_gender_bias/tests/test_process_text.py
@@ -6,6 +6,11 @@ from comp_sci_gender_bias.pipeline.process_text_utils import TokenTagger
 def test_TokenTagger():
 
     token_tagger = TokenTagger()
-    tags = token_tagger.tag("This is a test")
 
+    sentence = "London is in England"
+    tags = token_tagger.tag(sentence)
     assert len(tags) == 4
+    assert tags[0][2] == "NOUN"
+
+    tags = token_tagger.tag(sentence, convert_propn=False)
+    assert tags[0][2] == "PROPN"
