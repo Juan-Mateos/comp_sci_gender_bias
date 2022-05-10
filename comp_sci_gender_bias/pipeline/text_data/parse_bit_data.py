5ebae8ab8e3c1e5d2b346cbec955e98efb9f4caf
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-05-10 10:37:17 +0100

    addressed PR comments

--- comp_sci_gender_bias/pipeline/text_data/parse_bit_data.py
+++ comp_sci_gender_bias/pipeline/text_data/parse_bit_data.py
@@ -5,34 +5,37 @@ import json
 import os
 import docx
 
+from toolz import pipe
+
 from comp_sci_gender_bias import PROJECT_DIR
 
 DATA_DIR = f"{PROJECT_DIR}/inputs/data/"
 BIT_DIRS = ["bit_compsci_descriptions", "bit_geo_descriptions"]
 
 
-def getText(filename: str) -> str:
+def get_text(filename: str) -> str:
     """Reads and parses a doc file from BIT"""
 
     doc = docx.Document(filename)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return " ".join(fullText).strip()
+
+    return pipe(
+        [para.text for para in doc.paragraphs],
+        lambda full_text: " ".join(full_text).strip(),
+    )
 
 
 if __name__ == "__main__":
 
     for dir_, name in zip(BIT_DIRS, ["compsci", "geo"]):
         logging.info(f"parsing {name}")
+
         descr_path = os.path.join(DATA_DIR, dir_)
-        print(descr_path)
         docx_files = os.listdir(descr_path)
 
         # In this dict the keys are schools ids and the values the text descriptions
         # of a subject
         text_dict = {
-            file_name.split("_")[0]: getText(f"{descr_path}/{file_name}")
+            file_name.split("_")[0]: get_text(f"{descr_path}/{file_name}")
             for file_name in docx_files
         }
 
