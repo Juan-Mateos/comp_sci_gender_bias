83a6003540e000acb77d5482eefa8dfdf749733e
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-10 18:58:54 +0900

    Add getter for dfe combined school data

---
+++ comp_sci_gender_bias/getters/dfe_combined_school_data.py
@@ -0,0 +1,18 @@
+from comp_sci_gender_bias.pipeline.additional_school_info.combine_additional_school_info import (
+    save_combined_dfe_data,
+)
+from comp_sci_gender_bias import PROJECT_DIR
+import pandas as pd
+
+DFE_DIR = PROJECT_DIR / "inputs/data/dfe_school_info/"
+
+
+def dfe_combined_school_data():
+    """Load dataframe of combined department of
+    education school datasets. If it doesn't exist,
+    create the dataset.
+    """
+    combined_path = DFE_DIR / "dfe_combined_dataset.csv"
+    if not combined_path.exists():
+        save_combined_dfe_data()
+    return pd.read_csv(combined_path, index_col=0)
