4561f71dc2fb8f87aa719fd1d43c15c56c8c2f9a
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-16 19:08:42 +0900

    Add typehint for dfe_combined_school_data

--- comp_sci_gender_bias/getters/dfe_combined_school_data.py
+++ comp_sci_gender_bias/getters/dfe_combined_school_data.py
@@ -7,7 +7,7 @@ import pandas as pd
 COMBINED_DFE_PATH = PROJECT_DIR / "inputs/data/dfe_school_info/dfe_combined_dataset.csv"
 
 
-def dfe_combined_school_data():
+def dfe_combined_school_data() -> pd.DataFrame:
     """Load dataframe of combined department of education school
     datasets. This includes unique school reference number, location,
     ofsted rating, gender split, average attainment by gender.
