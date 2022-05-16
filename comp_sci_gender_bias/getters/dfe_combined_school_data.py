21ee6a2bc9e5aaad2216769c5e27c099d264a921
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-16 11:53:33 +0900

    Update dfe_combined_school_data function

--- comp_sci_gender_bias/getters/dfe_combined_school_data.py
+++ comp_sci_gender_bias/getters/dfe_combined_school_data.py
@@ -4,15 +4,15 @@ from comp_sci_gender_bias.pipeline.additional_school_info.combine_dfe_school_dat
 from comp_sci_gender_bias import PROJECT_DIR
 import pandas as pd
 
-DFE_DIR = PROJECT_DIR / "inputs/data/dfe_school_info/"
+COMBINED_DFE_PATH = PROJECT_DIR / "inputs/data/dfe_school_info/dfe_combined_dataset.csv"
 
 
 def dfe_combined_school_data():
-    """Load dataframe of combined department of
-    education school datasets. If it doesn't exist,
-    create the dataset.
+    """Load dataframe of combined department of education school
+    datasets. This includes unique school reference number, location,
+    ofsted rating, gender split, average attainment by gender.
+    The combined dataset will be created if it doesn't exist already.
     """
-    combined_path = DFE_DIR / "dfe_combined_dataset.csv"
-    if not combined_path.exists():
+    if not COMBINED_DFE_PATH.exists():
         save_combined_dfe_data()
-    return pd.read_csv(combined_path, index_col=0)
+    return pd.read_csv(COMBINED_DFE_PATH, index_col=0)
