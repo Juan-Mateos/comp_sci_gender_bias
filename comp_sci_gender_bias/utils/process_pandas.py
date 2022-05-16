97a4994004250bb4c0c19a954a8a2ef0c7061106
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-16 19:07:46 +0900

    Add functions and tests

--- comp_sci_gender_bias/utils/process_pandas.py
+++ comp_sci_gender_bias/utils/process_pandas.py
@@ -1,5 +1,5 @@
 """
-Processing utils
+Utils for processing pandas data
 """
 
 import pandas as pd
@@ -10,3 +10,16 @@ def cols_replace_space_and_lowercase(df: pd.DataFrame) -> pd.DataFrame:
     for column names in dataframe"""
     df.columns = df.columns.str.replace(" ", "_").str.lower()
     return df
+
+
+def remove_nonalphanum_lowercase(column: pd.Series) -> pd.Series:
+    """Return input column with non alphanumeric characters
+    removed and the text set to lowercase
+    """
+    return (
+        column.astype(str)
+        .str.strip()
+        .str.lower()
+        .str.replace("[^a-z0-9 ]", "")
+        .str.strip()
+    )
