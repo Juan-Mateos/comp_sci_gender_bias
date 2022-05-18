231e3d15eee10a097724646145d6fbf56479f3d6
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-19 00:19:43 +0900

    Update io functions to accept strings

--- comp_sci_gender_bias/utils/io.py
+++ comp_sci_gender_bias/utils/io.py
@@ -1,29 +1,37 @@
 import pathlib
-from typing import Any
+from typing import Any, Union
 import pickle
 
 
-def make_path_if_not_exist(path: pathlib.Path):
+def convert_str_to_pathlib_path(path: Union[pathlib.Path, str]) -> pathlib.Path:
+    """Convert string path to pathlib Path"""
+    return pathlib.Path(path) if type(path) is str else path
+
+
+def make_path_if_not_exist(path: Union[pathlib.Path, str]):
     """If the path does not exist, make it"""
+    path = convert_str_to_pathlib_path(path)
     if not path.exists():
         path.mkdir(parents=True)
 
 
-def save_pickle(obj: Any, save_path: pathlib.Path, file_name: str):
+def save_pickle(obj: Any, save_dir: Union[pathlib.Path, str], file_name: str):
     """Save object as pickle file
 
     Args:
         obj: Object to save as pickle file
-        save_path: Path to save file to
+        save_dir: Directory to save file to
         file_name: Name to give file
     """
-    make_path_if_not_exist(save_path)
-    with open(save_path / file_name, "wb") as out:
+    save_dir = convert_str_to_pathlib_path(save_dir)
+    make_path_if_not_exist(save_dir)
+    with open(save_dir / file_name, "wb") as out:
         pickle.dump(obj, out)
 
 
-def load_pickle(load_path: pathlib.Path) -> Any:
+def load_pickle(load_path: Union[pathlib.Path, str]) -> Any:
     """Load pickle file"""
+    load_path = convert_str_to_pathlib_path(load_path)
     with open(load_path, "rb") as inp:
         obj = pickle.load(inp)
     return obj
