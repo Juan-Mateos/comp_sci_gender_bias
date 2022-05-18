a32cc35da92825b003472c32bbb3646c3c93e6f5
Jack Rasala <jack.rasala@nesta.org.uk>
2022-05-18 17:50:35 +0900

    Add sentence embeddings to pipeline

---
+++ comp_sci_gender_bias/pipeline/sentence_embeddings/create_sentence_embeddings.py
@@ -0,0 +1,20 @@
+from comp_sci_gender_bias import PROJECT_DIR
+from comp_sci_gender_bias.getters.school_data import text_descriptions
+from comp_sci_gender_bias.utils.io import save_pickle
+from sentence_transformers import SentenceTransformer
+
+
+PRE_TRAINED_MODEL = "all-MiniLM-L6-v2"
+SUBJECTS = ["compsci", "geo"]
+EMBEDDING_SAVE_PATH = PROJECT_DIR / "outputs/embeddings"
+
+if __name__ == "__main__":
+    model = SentenceTransformer(PRE_TRAINED_MODEL)
+    for subject in SUBJECTS:
+        list_of_course_descriptions = list(text_descriptions(subject=subject).values())
+        embedding = model.encode(list_of_course_descriptions)
+        save_pickle(
+            obj=embedding,
+            save_path=EMBEDDING_SAVE_PATH,
+            file_name=f"{subject}_embedding.pkl",
+        )
