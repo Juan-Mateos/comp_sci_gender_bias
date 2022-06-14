bc4f26b2b3348f7cd8d59bee8ccf8d891ea401ef
Jack Rasala <jack.rasala@nesta.org.uk>
2022-06-14 11:43:40 +0100

    Add website cleaning functions to process_pandas

--- comp_sci_gender_bias/utils/process_pandas.py
+++ comp_sci_gender_bias/utils/process_pandas.py
@@ -23,3 +23,59 @@ def remove_nonalphanum_lowercase(column: pd.Series) -> pd.Series:
         .str.replace("[^a-z0-9 ]", "", regex=True)
         .str.strip()
     )
+
+
+def remove_fw_slash(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Remove '/' from the end of all rows in specified
+    df and col"""
+    df[col] = df[col].map(lambda x: str(x)[:-1] if str(x)[-1] == "/" else x)
+    return df
+
+
+def remove_http_https(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Remove http:// and https:// from the start of all rows in
+    specified df and col"""
+    df[col] = df[col].map(lambda x: str(x).replace("https://", ""))
+    df[col] = df[col].map(lambda x: str(x).replace("http://", ""))
+    return df
+
+
+def remove_new_para(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    "Remove \n from specified df and col"
+    df[col] = df[col].map(lambda x: str(x).replace("\n", ""))
+    return df
+
+
+def add_www(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Add www. to start of all rows in specified
+    df and col"""
+    df[col] = df[col].map(lambda x: f"www.{str(x)}" if str(x)[:4] != "www." else x)
+    return df
+
+
+def clean_website_col(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Clean website text to make it easier to join by:
+        - removing / from end of website
+        - remove http/https from start of website
+        - remove new paragraph
+        - add www. to start of website
+
+    Args:
+        df: Dataframe with column containing website text
+        col: Column containing website text
+
+    Returns:
+        Dataframe with cleaned website text
+    """
+    return (
+        df.pipe(remove_fw_slash, col)
+        .pipe(remove_http_https, col)
+        .pipe(remove_new_para, col)
+        .pipe(add_www, col)
+    )
+
+
+def percent_to_float(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Convert values from percent to float in specified df and col"""
+    df[col] = df[col].map(lambda x: float(str(x).strip("%")) / 100)
+    return df
