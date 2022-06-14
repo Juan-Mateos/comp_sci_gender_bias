d5ff79ca8a79391b416f0187a2f8dc7ffbd9ec22
Jack Rasala <jack.rasala@nesta.org.uk>
2022-06-14 17:00:47 +0100

    Add getter for school lvl bias with DfE data csv

---
+++ comp_sci_gender_bias/getters/school_lvl_bias_with_dfe_data.py
@@ -0,0 +1,14 @@
+from comp_sci_gender_bias import PROJECT_DIR
+import pandas as pd
+
+SCHOOL_LVL_BIAS_WITH_DFE_DATA_PATH = (
+    PROJECT_DIR / "outputs/data/scraped_data/semi_manual/course_descriptions.csv"
+)
+
+
+def school_lvl_bias_with_dfe_data() -> pd.DataFrame:
+    """Returns a dataframe of the scraped school descriptions
+    for Computer Science, Drama and Geography with a column
+    for mean gender differences for each subject and the
+    secondary DfE data"""
+    return pd.read_csv(SCHOOL_LVL_BIAS_WITH_DFE_DATA_PATH)
