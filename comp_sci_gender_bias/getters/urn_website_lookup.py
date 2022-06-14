b5d244a5c34595767884568bbf484b5e222a2f7a
Jack Rasala <jack.rasala@nesta.org.uk>
2022-06-14 12:05:30 +0100

    Update path for urn website lookup

--- comp_sci_gender_bias/getters/urn_website_lookup.py
+++ comp_sci_gender_bias/getters/urn_website_lookup.py
@@ -2,7 +2,8 @@ from comp_sci_gender_bias import PROJECT_DIR
 import pandas as pd
 
 URN_WEBSITE_LOOKUP_PATH = (
-    PROJECT_DIR / "inputs/data/get_information_schools_urn_website.csv"
+    PROJECT_DIR
+    / "inputs/data/urn_school_website_lookup/get_information_schools_urn_website.csv"
 )
 
 
