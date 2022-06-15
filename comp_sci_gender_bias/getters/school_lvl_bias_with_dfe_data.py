10f7e9ec80629765f5002e2b37523294c2ce0ae6
Jack Rasala <jack.rasala@nesta.org.uk>
2022-06-15 15:04:25 +0100

    Fix getter

--- comp_sci_gender_bias/getters/school_lvl_bias_with_dfe_data.py
+++ comp_sci_gender_bias/getters/school_lvl_bias_with_dfe_data.py
@@ -2,7 +2,7 @@ from comp_sci_gender_bias import PROJECT_DIR
 import pandas as pd
 
 SCHOOL_LVL_BIAS_WITH_DFE_DATA_PATH = (
-    PROJECT_DIR / "outputs/data/school_level/scraped_schools_urn_dfe.csv"
+    PROJECT_DIR / "outputs/school_level/scraped_schools_urn_dfe.csv"
 )
 
 
@@ -11,4 +11,4 @@ def school_lvl_bias_with_dfe_data() -> pd.DataFrame:
     for Computer Science, Drama and Geography with a column
     for mean gender differences for each subject and the
     secondary DfE data"""
-    return pd.read_csv(SCHOOL_LVL_BIAS_WITH_DFE_DATA_PATH)
+    return pd.read_csv(SCHOOL_LVL_BIAS_WITH_DFE_DATA_PATH, index_col=0)
